e4fbd4f793ea5cec5de77c6ec0ee4665a44a23cf
cyrille.trabi <cyrille.trabi@CLT-1613-L.mlcedi.com>
2017-10-02 18:46:49 +0000

    First commit

---
+++ src/main/java/ci/app/m1/AppM1Application.java
@@ -0,0 +1,12 @@
+package ci.app.m1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppM1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppM1Application.class, args);
+	}
+}
---
+++ src/test/java/ci/app/m1/AppM1ApplicationTests.java
@@ -0,0 +1,16 @@
+package ci.app.m1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppM1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
