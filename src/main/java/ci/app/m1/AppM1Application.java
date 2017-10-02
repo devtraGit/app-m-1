d92647eaeb318197eff79547894a1aa0a5f55a00
cyrille.trabi <cyrille.trabi@CLT-1613-L.mlcedi.com>
2017-10-02 18:54:43 +0000

    Add comment

--- src/main/java/ci/app/m1/AppM1Application.java
+++ src/main/java/ci/app/m1/AppM1Application.java
@@ -3,6 +3,11 @@ package ci.app.m1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author cyrille.trabi
+ *
+ */
 @SpringBootApplication
 public class AppM1Application {
 
